edee28b1cc5fb85c71dc8b80a86bff0057f0b7c8
gordo <mrgordon1111@gmail.com>
2025-05-28 06:25:18 +0200

    Добавил DTO и маппинг

--- main-service/src/main/java/ru/practicum/rating/mapper/RatingMapper.java
+++ main-service/src/main/java/ru/practicum/rating/mapper/RatingMapper.java
@@ -1,4 +1,32 @@
 package ru.practicum.rating.mapper;
 
+import ru.practicum.exceptions.ValidationRequestException;
+import ru.practicum.rating.dto.RatingDto;
+import ru.practicum.rating.model.Rating;
+import ru.practicum.rating.model.RatingType;
+
 public class RatingMapper {
+
+    public static Rating toRating(RatingDto dto) {
+        if (dto == null) return null;
+
+        RatingType type;
+        try {
+            type = RatingType.valueOf(dto.getRating());
+        } catch (IllegalArgumentException e) {
+            throw new ValidationRequestException("Недопустимое значение рейтинга: " + dto.getRating());
+        }
+
+        return Rating.builder()
+                     .rating(type)
+                     .build();
+    }
+
+    public static RatingDto toRatingDto(Rating rating) {
+        if (rating == null) return null;
+        return RatingDto.builder()
+                        .id(rating.getId())
+                        .rating(rating.getRating().name())
+                        .build();
+    }
 }
